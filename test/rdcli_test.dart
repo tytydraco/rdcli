d8d03e4822a5d2212e6b9d6d2096bba91e90df81
Tyler Nijmeh <tylernij@gmail.com>
2025-09-04 18:04:35 -0700

    Externalize api stuff
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- test/rdcli_test.dart
+++ test/rdcli_test.dart
@@ -5,28 +5,7 @@ import 'package:rdcli/rdcli.dart';
 import 'package:rdcli/src/exceptions/add_magnet_exception.dart';
 import 'package:test/test.dart';
 
-/// Test magnet of an Arch Linux ISO.
-/// https://webtorrent.io/free-torrents
-const _testMagnet =
-    'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent';
-
-/// The SHA256 checksum for [_testMagnet].
-const _testMagnetSha256 =
-    '0fe0f7b56d87a97532e74a56d60315b3c27f02be29139f083fd6513d3cbf57fa';
-
-final String _apiKey = _getApiKey();
-
-String _getApiKey() {
-  final environment = Platform.environment;
-  if (!environment.containsKey('RDCLI_API_KEY')) {
-    stderr.writeln(
-      'Environmental variable `RDCLI_API_KEY` must be set to run tests.',
-    );
-    exit(1);
-  }
-
-  return Platform.environment['RDCLI_API_KEY']!;
-}
+import 'config.dart';
 
 void main() {
   Directory? testDir;
@@ -40,7 +19,7 @@ void main() {
   group(
     'Rdcli intended behavior',
     () {
-      final rdcli = Rdcli(magnet: _testMagnet, apiKey: _apiKey);
+      final rdcli = Rdcli(magnet: testMagnet, apiKey: apiKey);
       File? file;
 
       test('Verify test torrent downloads', () async {
@@ -53,7 +32,7 @@ void main() {
         final inputStream = file!.openRead();
         final digest = await sha256.bind(inputStream).first;
 
-        expect(digest.toString(), _testMagnetSha256);
+        expect(digest.toString(), testMagnetSha256);
       });
     },
     timeout: const Timeout(Duration(minutes: 5)),
@@ -64,7 +43,7 @@ void main() {
     'Rdcli error behavior',
     () {
       test('Bad magnet link', () async {
-        final rdcli = Rdcli(magnet: '12345', apiKey: _apiKey);
+        final rdcli = Rdcli(magnet: '12345', apiKey: apiKey);
         expect(
           () => rdcli.download(testDir!),
           throwsA(isA<AddMagnetException>()),
@@ -72,7 +51,7 @@ void main() {
       });
 
       test('Bad API key', () async {
-        final rdcli = Rdcli(magnet: _testMagnet, apiKey: '12345');
+        final rdcli = Rdcli(magnet: testMagnet, apiKey: '12345');
         expect(
           () => rdcli.download(testDir!),
           throwsA(isA<AddMagnetException>()),
